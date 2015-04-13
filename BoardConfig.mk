2fe8bdd13611be38a90122fc9c077bd923f829a3
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-04-13 14:59:01 +0200

    move prebuilt kernel to vendor/sony/kernel
    by default we enable kernel build from source
    
    if vendor/sony/kernel is included in the android tree
    we use prebuilt kernel, if folder is not prezent then
    kernel is built from source
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,3 +100,6 @@ EXTENDED_FONT_FOOTPRINT := true
 # Enable dex-preoptimization to speed up first boot sequence
 WITH_DEXPREOPT := true
 
+BUILD_KERNEL := true
+-include vendor/sony/kernel/KernelConfig.mk
+
