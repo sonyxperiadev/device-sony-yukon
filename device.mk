3024ef32df689e6ab3f90dc22c74f11488ffe223
Alexis Rico <me@sferadev.com>
2015-11-17 16:23:34 +0000

    yukon: Beautify and mainstream SONY_ROOT
    - Add missing spaces not fixed on cff365a06df6693db725c5c8b0f6d3a6b2eb90a4.
    - Expanding the variable on declaration time is trivial here, let's just follow the pattern of the other families.

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SONY_ROOT:=device/sony/yukon/rootdir
+SONY_ROOT = device/sony/yukon/rootdir
 
 SOMC_PLATFORM := yukon
 
