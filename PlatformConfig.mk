7e38600b64ba9a8eb30625ee02a6c7654ddd1d3d
Adam Farden <adam@farden.cz>
2016-06-16 00:39:42 +0200

    Move sysfs and props for cpuquiet hotplugging
    Core limiting via msm_performance works well, but cpuquiet absolutely
    hates it. rqbalance is not aware of msm_performance so it spams dmesg
    because it cannot online a core for 'unknown' reasons.
    
    We can achieve the same result using cpuquiet which rqbalance is
    aware of. We can also have two sources of core limiting, power
    profile and thermal.
    
    We also need to tune rqbalance and cpuquiet per platform so also
    move their system props.
    
    Signed-off-by: Adam Farden <adam@farden.cz>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -60,6 +60,9 @@ BOARD_HAVE_BLUETOOTH_QCOM := true
 # NFC
 NFC_NXP_CHIP_TYPE := PN547C2
 
+# Props for hotplugging
+TARGET_SYSTEM_PROP += device/sony/yukon/system.prop
+
 # SELinux
 BOARD_SEPOLICY_DIRS += device/sony/yukon/sepolicy
 
